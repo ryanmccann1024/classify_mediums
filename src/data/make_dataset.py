6e3879cecc1ad8c68d4ee3c4015daabbb3b28418
Sage Lyon <sage.clyon@gmail.com>
2023-04-04 15:59:18 -0400

    Modified clean_string() to use regex

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,5 @@
 import os
+import re
 import requests
 import time
 import random
@@ -73,7 +74,7 @@ def clean_string(dirty_str: str):
     """
     Formats a string to snakecase.
     """
-    clean_str = dirty_str.lower().replace(',', '').replace(' ', '_').replace('.', '').replace('-', '_').replace("'", '').replace('"', '')
+    clean_str = re.sub('\W+', '', dirty_str)
     return clean_str
 
 
